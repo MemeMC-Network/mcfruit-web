6762d2ea883ed7b2dad5a490f2369043addf8434
MemeMC <149332208+MemeMC-Network@users.noreply.github.com>
2025-11-15 19:26:57 -0500

    bye bye the too much qol

--- js/script.js
+++ js/script.js
@@ -321,40 +321,6 @@ document.addEventListener('DOMContentLoaded', () => {
             
             setTimeout(() => ripple.remove(), 600);
         });
-        
-        // Magnetic button effect
-        button.addEventListener('mousemove', function(e) {
-            const rect = this.getBoundingClientRect();
-            const x = e.clientX - rect.left - rect.width / 2;
-            const y = e.clientY - rect.top - rect.height / 2;
-            
-            this.style.transform = `translate(${x * 0.15}px, ${y * 0.15}px)`;
-        });
-        
-        button.addEventListener('mouseleave', function() {
-            this.style.transform = 'translate(0, 0)';
-        });
-    });
-    
-    // Card tilt effect on mouse move
-    document.querySelectorAll('.feature-card, .gamemode-card, .staff-card').forEach(card => {
-        card.addEventListener('mousemove', function(e) {
-            const rect = this.getBoundingClientRect();
-            const x = e.clientX - rect.left;
-            const y = e.clientY - rect.top;
-            
-            const centerX = rect.width / 2;
-            const centerY = rect.height / 2;
-            
-            const rotateX = (y - centerY) / 10;
-            const rotateY = (centerX - x) / 10;
-            
-            this.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale3d(1.05, 1.05, 1.05)`;
-        });
-        
-        card.addEventListener('mouseleave', function() {
-            this.style.transform = 'perspective(1000px) rotateX(0) rotateY(0) scale3d(1, 1, 1)';
-        });
     });
 });
 
